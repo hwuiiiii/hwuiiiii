14cdb41fdd9a81cbf116a58a147af022fcf9fb8d
Hannah <hy5vb@naver.com>
2021-11-30 05:29:46 +0900

    20211129

--- maeilcoding/3-1.py
+++ maeilcoding/3-1.py
@@ -1,10 +1,10 @@
 n = 1260
 count = 0
 
-coin_types = [500, 10, 50, 10]
+coin_types = [500, 100, 50, 10]
 
 for coin in coin_types:
     count += n // coin
     n %= coin
 
-print(count)    
+print(count)
