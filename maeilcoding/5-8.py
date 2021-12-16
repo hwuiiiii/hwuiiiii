19c428243b40102423311bf68dc7fa7f1ca5e864
Hannah <hy5vb@naver.com>
2021-12-16 15:39:10 +0900

    20211216

---
+++ maeilcoding/5-8.py
@@ -0,0 +1,26 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# make graph using list method
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# instantiate 1 dim list (corresponding to graph)
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+print()
