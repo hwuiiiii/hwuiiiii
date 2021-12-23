6559902dcf0334e3f303e14a8e62824e5077bb4e
Hannah <hy5vb@naver.com>
2021-12-23 17:22:18 +0900

    20211223

---
+++ maeilcoding/6-3.py
@@ -0,0 +1,10 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): # continue loop backwards(-1) starting from index i to 0
+        if array[j] < array[j - 1]: # compare index with index on left side
+            array[j], array[j - 1] = array[j - 1], array[j] # swap if index is smaller than index on left side
+        else:
+            break
+
+print(array)
