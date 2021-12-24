cfbbcdd8dffbd4786ccfd4d19ff1d8b225636e94
Hannah <hy5vb@naver.com>
2021-12-24 09:19:39 +0900

    20211224

--- maeilcoding/6-4.py
+++ maeilcoding/6-4.py
@@ -8,5 +8,19 @@ def quick_sort(array, start, end):
 	right = end
 	
 	while left <= right:
-		
-		while left <= end
+		# repeat until left index value is larger than pivot
+		while left <= end and array[left] <= array[pivot]:
+			left += 1
+		# repeat until right index value is smaller than pivot
+		while right > start and array[right] >= array[pivot]:
+			right -= 1
+		if left > right: # swap right index value with pivot if left index greater than right index
+			array[right], array[pivot] = array[pivot], array[right]
+		else: # swap left and right index values if right index is equal to or greater then left index
+			array[left], array[right] = array[right], array[left]
+	# quick sort for both sides after split
+	quick_sort(array, start, right - 1)
+	quick_sort(array, right + 1, end)
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
