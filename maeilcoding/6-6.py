23543ee5672ec0ec4cc51d4682d9ced3ec8d034f
Hannah <hy5vb@naver.com>
2021-12-26 12:31:57 +0900

    20211226

---
+++ maeilcoding/6-6.py
@@ -0,0 +1,12 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+
+# create list of array length (init with 0)
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+  count[array[i]] += 1 # count for each number in array
+  
+for i in range(len(count)): # for each number in count list
+  for j in range(count[i]): 
+    print(i, end=" ") # print i element j times
+  
